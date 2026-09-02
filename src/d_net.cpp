Boondorl/gzdoom

Fixed networking messages
Tweaks to loading. Added support for specifying bot thinker classes.

src/d_net.cpp
@@ -2280,16 +2280,23 @@ void Net_DoCommand (int type, uint8_t **stream, int player)
 		break;
 
 	case DEM_ADDBOT:
-		DBotManager::TryAddBot(primaryLevel, ReadByte(stream), ReadStringConst(stream));
+	{
+		const unsigned int playerID = ReadByte(stream);
+		const FName botID = ReadStringConst(stream);
+		DBotManager::TryAddBot(primaryLevel, playerID, botID);
 		break;
+	}
 
 	case DEM_KILLBOTS:
 		DBotManager::RemoveAllBots(primaryLevel);
 		break;
 
 	case DEM_KILLBOT:
-		DBotManager::RemoveBot(primaryLevel, ReadByte(stream));
+	{
+		const unsigned int playerID = ReadByte(stream);
+		DBotManager::RemoveBot(primaryLevel, playerID);
 		break;
+	}
 
 	case DEM_CENTERVIEW:
 		players[player].centering = true;
@@ -2751,7 +2758,11 @@ void Net_SkipCommand (int type, uint8_t **stream)
 			break;
 
 		case DEM_ADDBOT:
-			skip = strlen ((char *)(*stream + 1)) + 6;
+			skip = strlen ((char *)(*stream + 1)) + 2;
+			break;
+
+		case DEM_KILLBOT:
+			skip = 1;
 			break;
 
 		case DEM_GIVECHEAT:

Boondorl/UZDoom

src/d_net.cpp
@@ -2707,16 +2707,23 @@ void Net_DoCommand(int cmd, TArrayView<uint8_t>& stream, int player)
 		break;
 
 	case DEM_ADDBOT:
-		DBotManager::TryAddBot(primaryLevel, ReadInt8(stream), ReadStringConst(stream));
+	{
+		const unsigned int playerID = ReadInt8(stream);
+		const FName botID = ReadStringConst(stream);
+		DBotManager::TryAddBot(primaryLevel, playerID, botID);
 		break;
+	}
 
 	case DEM_KILLBOTS:
 		DBotManager::RemoveAllBots(primaryLevel);
 		break;
 
 	case DEM_KILLBOT:
-		DBotManager::RemoveBot(primaryLevel, ReadInt8(stream));
+	{
+		const unsigned int playerID = ReadInt8(stream);
+		DBotManager::RemoveBot(primaryLevel, playerID);
 		break;
+	}
 
 	case DEM_CENTERVIEW:
 		players[player].centering = true;
@@ -3211,7 +3218,11 @@ void Net_SkipCommand(int cmd, TArrayView<uint8_t>& stream)
 			break;
 
 		case DEM_ADDBOT:
-			skip = strlen((char *)(stream.Data() + 1)) + 6;
+			skip = strlen ((char *)(stream.Data() + 1)) + 2;
+			break;
+
+		case DEM_KILLBOT:
+			skip = 1;
 			break;
 
 		case DEM_GIVECHEAT: